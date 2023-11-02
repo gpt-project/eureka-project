83afe18c5b3670800a6d9812c2b8d2209024e3fc
parksangwoo <b01056721617@gmail.com>
2023-11-02 09:53:39 +0900

    환경변수 관리

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { requestGPTApi } from './service';
+import express, { Request, Response } from "express";
+import { requestGPTApi } from "./service";
 
 const app = express();
 
@@ -7,14 +7,14 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.post("/api", async (req, res) => {
-    const thema = req.body.thema;
-    const period = req.body.travelPeriod;
-    const response = await requestGPTApi(thema, period);
+app.post("/api", async (req: Request, res: Response) => {
+  const thema = req.body.thema;
+  const period = req.body.travelPeriod;
+  const response = await requestGPTApi(thema, period);
 
-    res.send(response)
-})
+  res.send(response);
+});
 
 app.listen(PORT, () => {
-    console.log('server started');
+  console.log("server started");
 });
